b57c03497d88bebd393944f1ecd256b18d38363b
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2023-09-22 13:38:47 +0700

    tutorial: server starting config

---
+++ nodejs_tutorial/api/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { createServer } = require('http');
+const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
+const typeDefs = require('./schemas');
+const resolvers = require('./resolvers');
+const context = require('./contexts');
+const app = express();
+
+app.use(cors());
+
+const startServer = async () => {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context,
+        introspection: true,
+        playground: {
+            settings: {
+                'schema.polling.enable': false,
+            },
+        },
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app, path: '/api' });
+}
+
+startServer();
+
+const server = createServer(app);
+
+module.exports = server;
